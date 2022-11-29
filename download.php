bb2087c3fef3f3b1bd6836b311c8187cf6d77976
Stefan Schilling <st-schilling@gmx.de>
2022-11-29 22:22:43 +0100

    AOSP Security Updates November 2022: updated download links

--- download.php
+++ download.php
@@ -54,6 +54,55 @@ include_once('utils.php');
                     </p>
 
                     <h3 class="textt main-font center white-text">Motorola Moto G5S Plus (sanders)</h3>
+                    <p class="flow-text main-font paragraph text3 text-lighten-3">2022-11-01: My unofficial image, including
+                        the <a href="https://source.android.com/security/bulletin/2022-11-01">AOSP Security Updates from November 2022</a>:
+                        <a href="distribution/sanders/unofficial/arrow-11.0/vanilla/Arrow-v11.0-sanders-UNOFFICIAL-OTA-20221101-VANILLA.zip">Arrow-v11.0-sanders-UNOFFICIAL-OTA-20221101-VANILLA.zip</a>.
+                        <br>
+                        Source code: <a href="https://github.com/st-schilling">Tag 11.0.0_r61</a>, as defined by
+                        <a href="https://github.com/st-schilling/android_manifest/releases/tag/11.0.0_r61">the manifest repo.</a>
+                    </p>
+                    <p class="flow-text main-font paragraph text3 text-lighten-3">2022-10-01: My unofficial image, including
+                        the <a href="https://source.android.com/security/bulletin/2022-10-01">AOSP Security Updates from October 2022</a>:
+                        <a href="distribution/sanders/unofficial/arrow-11.0/vanilla/Arrow-v11.0-sanders-UNOFFICIAL-OTA-20221001-VANILLA.zip">Arrow-v11.0-sanders-UNOFFICIAL-OTA-20221001-VANILLA.zip</a>.
+                        <br>
+                        Source code: <a href="https://github.com/st-schilling">Tag 11.0.0_r60</a>, as defined by
+                        <a href="https://github.com/st-schilling/android_manifest/releases/tag/11.0.0_r60">the manifest repo.</a>
+                    </p>
+                    <p class="flow-text main-font paragraph text3 text-lighten-3">2022-09-01: My unofficial image, including
+                        the <a href="https://source.android.com/security/bulletin/2022-09-01">AOSP Security Updates from September 2022</a>:
+                        <a href="distribution/sanders/unofficial/arrow-11.0/vanilla/Arrow-v11.0-sanders-UNOFFICIAL-OTA-20220901-VANILLA.zip">Arrow-v11.0-sanders-UNOFFICIAL-OTA-20220901-VANILLA.zip</a>.
+                        <br>
+                        Source code: <a href="https://github.com/st-schilling">Tag 11.0.0_r59</a>, as defined by
+                        <a href="https://github.com/st-schilling/android_manifest/releases/tag/11.0.0_r59">the manifest repo.</a>
+                    </p>
+                    <p class="flow-text main-font paragraph text3 text-lighten-3">2022-08-01: My unofficial image, including
+                        the <a href="https://source.android.com/security/bulletin/2022-08-01">AOSP Security Updates from August 2022</a>:
+                        <a href="distribution/sanders/unofficial/arrow-11.0/vanilla/Arrow-v11.0-sanders-UNOFFICIAL-OTA-20220801-VANILLA.zip">Arrow-v11.0-sanders-UNOFFICIAL-OTA-20220801-VANILLA.zip</a>.
+                        <br>
+                        Source code: <a href="https://github.com/st-schilling">Tag 11.0.0_r58</a>, as defined by
+                        <a href="https://github.com/st-schilling/android_manifest/releases/tag/11.0.0_r58">the manifest repo.</a>
+                    </p>
+                    <p class="flow-text main-font paragraph text3 text-lighten-3">2022-07-01: My unofficial image, including
+                        the <a href="https://source.android.com/security/bulletin/2022-07-01">AOSP Security Updates from July 2022</a>:
+                        <a href="distribution/sanders/unofficial/arrow-11.0/vanilla/Arrow-v11.0-sanders-UNOFFICIAL-OTA-20220701-VANILLA.zip">Arrow-v11.0-sanders-UNOFFICIAL-OTA-20220701-VANILLA.zip</a>.
+                        <br>
+                        Source code: <a href="https://github.com/st-schilling">Tag 11.0.0_r57</a>, as defined by
+                        <a href="https://github.com/st-schilling/android_manifest/releases/tag/11.0.0_r57">the manifest repo.</a>
+                    </p>
+                    <p class="flow-text main-font paragraph text3 text-lighten-3">2022-06-01: My unofficial image, including
+                        the <a href="https://source.android.com/security/bulletin/2022-06-01">AOSP Security Updates from June 2022</a>:
+                        <a href="distribution/sanders/unofficial/arrow-11.0/vanilla/Arrow-v11.0-sanders-UNOFFICIAL-OTA-20220601-VANILLA.zip">Arrow-v11.0-sanders-UNOFFICIAL-OTA-20220601-VANILLA.zip</a>.
+                        <br>
+                        Source code: <a href="https://github.com/st-schilling">Tag 11.0.0_r56</a>, as defined by
+                        <a href="https://github.com/st-schilling/android_manifest/releases/tag/11.0.0_r56">the manifest repo.</a>
+                    </p>
+                    <p class="flow-text main-font paragraph text3 text-lighten-3">2022-05-01: My unofficial image, including
+                        the <a href="https://source.android.com/security/bulletin/2022-05-01">AOSP Security Updates from May 2022</a>:
+                        <a href="distribution/sanders/unofficial/arrow-11.0/vanilla/Arrow-v11.0-sanders-UNOFFICIAL-OTA-20220501-VANILLA.zip">Arrow-v11.0-sanders-UNOFFICIAL-OTA-20220501-VANILLA.zip</a>.
+                        <br>
+                        Source code: <a href="https://github.com/st-schilling">Tag 11.0.0_r55</a>, as defined by
+                        <a href="https://github.com/st-schilling/android_manifest/releases/tag/11.0.0_r55">the manifest repo.</a>
+                    </p>
                     <p class="flow-text main-font paragraph text3 text-lighten-3">2022-04-05: My unofficial image, including
                         the <a href="https://source.android.com/security/bulletin/2022-04-01">AOSP Security Updates from April 2022</a>:
                         <a href="distribution/sanders/unofficial/arrow-11.0/vanilla/Arrow-v11.0-sanders-UNOFFICIAL-OTA-20220405-VANILLA.zip">Arrow-v11.0-sanders-UNOFFICIAL-OTA-20220405-VANILLA.zip</a>.
