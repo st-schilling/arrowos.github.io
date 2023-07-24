43c6cb18283848392db1569c41864a2d7f75d330
Stefan Schilling <st-schilling@gmx.de>
2023-07-24 19:47:36 +0200

    AOSP Security Updates July 2023: updated references for AOSP Security Release r69 of July 2023

--- download.php
+++ download.php
@@ -54,6 +54,13 @@ include_once('utils.php');
                     </p>
 
                     <h3 class="textt main-font center white-text">Motorola Moto G5S Plus (sanders)</h3>
+                    <p class="flow-text main-font paragraph text3 text-lighten-3">2023-07-01: My unofficial image, including
+                        the <a href="https://source.android.com/security/bulletin/2023-07-01">AOSP Security Updates from July 2023</a>:
+                        <a href="distribution/sanders/unofficial/arrow-11.0/vanilla/Arrow-v11.0-sanders-UNOFFICIAL-OTA-20230701-VANILLA.zip">Arrow-v11.0-sanders-UNOFFICIAL-OTA-20230701-VANILLA.zip</a>.
+                        <br>
+                        Source code: <a href="https://github.com/st-schilling">Tag 11.0.0_r69</a>, as defined by
+                        <a href="https://github.com/st-schilling/android_manifest/releases/tag/11.0.0_r69">the manifest repo.</a>
+                    </p>
                     <p class="flow-text main-font paragraph text3 text-lighten-3">2023-06-01: My unofficial image, including
                         the <a href="https://source.android.com/security/bulletin/2023-06-01">AOSP Security Updates from June 2023</a>:
                         <a href="distribution/sanders/unofficial/arrow-11.0/vanilla/Arrow-v11.0-sanders-UNOFFICIAL-OTA-20230601-VANILLA.zip">Arrow-v11.0-sanders-UNOFFICIAL-OTA-20230601-VANILLA.zip</a>.
